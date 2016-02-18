0dbd82fad64f7c638910483e9f9b911b2ab17e2d
Philip Seger <seger.philip@gmail.com>
2016-02-18 16:52:14 -0500

    inclass notes, no code yet

---
+++ Classes/5-Tutorial.ml
@@ -0,0 +1,95 @@
+(* 
+Map, Filter, fold-right ==> higher-order functions
+    |
+fold-left (variant on fold-right) ~ steps (sort of recurses the same way)
+ *)
+
+(* Finite automata
+    ~(deterministic) FA
+    |||
+  accept exactly the regular languages
+                      |||
+                    languages described by regular expressions
+
+automata for palindrome (impossible), remembering has to be saved in the structure
+      |- not regular
+{a^n b^n | n>= 0} = {E, ab, aabb, aaabbb} not regular, can't remember n
+ *)
+
+(* FA: finite control
+
+Turing machine: finite control + RAM (SAM? sequential, not random)
+                                  |- literature calls it "tape" (old school)
+tape looks like:
+__________________________________
+| - | a | b | c | d | _ | _ |  
+----------------------------------
+              ^  <-tape head pointing at cell with c in it
+  Intuition:
+    1 - tape starts with the input string on it
+    2 - tape head on left most cell
+    3 - control starts in start state
+    4 - transition based on current state + the symbbol under the tape head
+    5 - continue until you reach an accept or reject state
+
+  Transitions:
+    from the current state & symbol under the tape head*:
+      - move to a new state
+      - write a new symbol under the tape head
+      - move the tape left or right
+
+    *-(T)uring (M)achines are deterministic
+
+    transitions look like this:
+      p ----> q       <-transition
+          a/b, L (or R)           <-under tape head, if a converts to b, then moves tape head L (or R)
+
+Example1:
+  TM that accepts all strings over {a, b} with an even number of a's
+    (tape looks like - a b a b _ _ _ )
+                     |- left most tape start, looks like |-
+  start -> O -(|-/|-,R) -> O - (a/a,R) -> O - (a/a,R) - < move back to 2nd
+                            ^             ^
+                            |             |
+                          (b/b,R)      (b/b,R)
+                            |             |
+                            v             v
+                       (loop back up)  (loop back up)
+      
+      ends on space character from tape, which goes from 2nd to accept statement, and (_/_,R)
+          else (_/_,R) and reject
+      any transition not given in the diagram goes to the reject state
+real drawing of ^^^ in moleskin at #1
+
+ *)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
