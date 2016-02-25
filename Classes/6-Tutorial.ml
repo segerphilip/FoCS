558957d76415b54ff08c7ad07372f3f847be914b
Philip Seger <seger.philip@gmail.com>
2016-02-25 16:09:39 -0500

    exam information

---
+++ Classes/6-Tutorial.ml
@@ -0,0 +1,9 @@
+(* 
+Next lecture: March 10, small inclass midterm (email w/ details soon)
+is a language regular. if regular, design a expression for this language.
+  that level of complexity
+
+
+
+
+*)
