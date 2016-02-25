947260e4db1691f7c737850d2e68d02d00920379
Philip Seger <seger.philip@gmail.com>
2016-02-25 17:13:37 -0500

    extra stuff, check notebooks

--- Classes/6-Tutorial.ml
+++ Classes/6-Tutorial.ml
@@ -1,9 +1,25 @@
-(* 
+(*  
 Next lecture: March 10, small inclass midterm (email w/ details soon)
 is a language regular. if regular, design a expression for this language.
   that level of complexity
+*)
 
+(* 
+limits of undecideability/decidability - church-turing thesis
 
+question is are there decision problems that are not computable: answer is YES
+  there must be at least one, b/c there are more languages than turing machines
 
+Def: 
+  Two sets A, B are equipollent (have the same "size") if you can match 
+  the elements of A with the elements of B 
+  (i.e. there exists a function F from A to -> B that is 1 to 1 and onto)
+
+Def:
+  A <~(smaller or equal) B if there is a C subset of B such that A ~~ C 
+      =_ there is a one to one function from A -> B
+      =_ there is an onto function from B -> A
+
+Check notebook @ 2
 
 *)
